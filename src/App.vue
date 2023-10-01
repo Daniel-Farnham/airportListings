65450c1ea9abc2fe4c1af2329f319bb154a3b45b
D.Farnham <danjf1210@gmail.com>
2023-10-01 21:38:10 +1100

    Fix: Returned list of Airports in Australia

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,13 @@
         </option>
       </select>
     </div>
-    
+    <div v-if="firstAirport">
+      <h2>Information for the First Airport</h2>
+      <p><strong>Name:</strong> {{ firstAirport.name }}</p>
+      <p><strong>ICAO Code:</strong> {{ firstAirport.icao_code }}</p>
+      <p><strong>IATA Code:</strong> {{ firstAirport.iata_code || 'N/A' }}</p>
+      <p><strong>Country Code:</strong> {{ firstAirport.country_code }}</p>
+    </div>
     <div>
       <h2>Arrivals</h2>
       <!-- Container for arrivals will go here -->
@@ -31,11 +37,17 @@ export default {
     return {
       selectedAirport: '',
       airports: [
-        { name: 'Sydney Airport', code: 'SYD' },
-        { name: 'Melbourne Airport', code: 'MEL' },
+        // { name: 'Sydney Airport', code: 'SYD' },
+        // { name: 'Melbourne Airport', code: 'MEL' },
         //... other Australian airports
       ],
     };
+  },
+  computed: {
+    firstAirport() {
+      // Returns the first airport from the airports array, or null if the array is empty
+      return this.airports.length > 0 ? this.airports[0] : null;
+    },
   },
   mounted() {
     this.fetchData();
@@ -43,12 +55,15 @@ export default {
   methods: {
     async fetchData() {
       try {
-        const response = await apiService.apiCall('ping', {param1: 'value1'});
-        console.log(response);
+        const returnedData = await apiService.apiCall('airports', { country_code: 'AU' });
+        console.log(returnedData);
+        this.airports = returnedData.response; 
       } catch (error) {
-        console.error('There was an error!', error);
+        //console.error('There was an error!', error);
       }
     },
   },
 };
+
+
 </script>
